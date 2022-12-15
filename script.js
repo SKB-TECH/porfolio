c845e46435291c1f74d021808afcdb69781251e9
Shako Kinyamba <shakokinyamba201@gmail.com>
2022-12-15 11:18:30 +0200

    feat:send email

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 $(document).ready(function () {
     $(window).scroll(function () {
         if (this.scrollY > 20) {
@@ -68,3 +70,27 @@ $(document).ready(function () {
         }
     })
 });
+
+
+
+/* SmtpJS.com - v3.0.0 */
+// var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
+
+btn.addEventListener('click', (e) => {
+
+    const email = document.getElementById('email').value
+    const name = document.getElementById('name').value
+    const subject = document.getElementById('subject').value
+    const message = document.getElementById('content').value
+    const btn = document.getElementById('btn');
+    e.preventDefault()
+
+    emailjs.send("service_32blw6g", "template_21ch5jk", {
+        from_name: name,
+        to_name: "shako",
+        message: message,
+    });
+
+})
+
